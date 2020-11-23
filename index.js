5e36b8314464199bc4d5fca2c006088b842a660b
cjbrock <corinna@flatironschool.com>
2020-11-23 09:33:14 -0500

    post study group code

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
     loadFormlistener()
+    buttonEvent()
+    clickEvent()
+    mouseOverEvent()
 })
 
 function loadFormlistener(){
@@ -16,36 +19,74 @@ function loadFormlistener(){
         attachPost(htmlPost)
         clearForm()
     })  
+}
 
 
-    function getInfo(event){
-        return {
-            title: event.target.querySelector("#title").value,
-            author: event.target.querySelector("#author").value,
-            content: event.target.querySelector("#content").value
-        }
+function getInfo(event){
+    return {
+        title: event.target.querySelector("#title").value,
+        author: event.target.querySelector("#author").value,
+        content: event.target.querySelector("#content").value
     }
+}
 
-    function postHtml(post){
-        return `
-        <div class="card">
-            <div class="card-content">
-                <span class="card-title">${post.title}</span>
-                <p>${post.author}</p>
-                <p>${post.content}</p>
-                <button class="colorButton">Change Background</button>
-            </div>
+function postHtml(post){
+    return `
+    <div class="card">
+        <div class="card-content">
+            <span class="card-title">${post.title}</span>
+            <p>${post.author}</p>
+            <p>${post.content}</p>
+            <button class="colorButton">Change Background</button>
         </div>
-        `
-    }
+    </div>
+    `
+}
 
-    const attachPost = function(post){
-        document.querySelector(".post-lists").innerHTML += post
-    }
+const attachPost = function(post){
+    document.querySelector(".post-lists").innerHTML += post
+}
 
-    const clearForm = () => {
-        document.getElementById("title").value = ""
-        document.getElementById("author").value = ""
-        document.getElementById("content").value = ""
+const clearForm = () => {
+    document.getElementById("title").value = ""
+    document.getElementById("author").value = ""
+    document.getElementById("content").value = ""
+}
+
+function buttonEvent(){
+    const allPosts = document.querySelector(".post-lists")
+    const colors = ["orange", "brown", "gold", "red", "green", "black"]
+    let index = 0
+    const maxIndex = colors.length
+    allPosts.addEventListener("click", function(e){
+        if (e.target.className === "colorButton"){
+            e.target.parentElement.parentElement.style.backgroundColor = colors[index++]
+            if(index == maxIndex){
+                index = 0;
+            }
+        }
+    })
+}
+
+const christmasColors = ["red", "lightgreen", "white",  "silver", "blue", "gold"]
+let index = 0
+const maxIndex = christmasColors.length
+
+const changeColor = title => {
+    title.style.color = christmasColors[index++]
+    if(index == maxIndex){
+        index = 0;
     }
+}
+
+function clickEvent(){
+    const title = document.querySelector(".post-lists h3")
+    title.addEventListener("click", function(){
+        changeColor(title)
+    })
+}
+
+function mouseOverEvent(){
+    const head = document.querySelector("h1")
+    head.addEventListener("mouseover", ()=> changeColor(head))
 }
